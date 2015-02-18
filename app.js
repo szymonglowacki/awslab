bca6feebf336a62b5618f056264757accc6cbe86
Szymon Głowacki <sznglowacki@gmail.com>
2015-02-18 18:38:50 +0100

    lab2, lab3

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var lab1_1 = require("./lab/lab1_1").lab;
 var example_1 = require("./example_1").lab;
 var lab2_1 = require("./lab/lab2_1").lab;
-var lab2_2 = require("./lab/lab2_2").lab
+var lab2_2 = require("./lab/lab2_2").lab;
+var lab3_1 = require("./lab/lab3_1").lab;
 
 var PORT = 8080;
 
@@ -11,7 +12,8 @@ var urlMap = [
 	{path: "/digest", action: lab1_1},	
 	{path: "/example_1", action: example_1}, 
 	{path: "/lab2_1", action: lab2_1},
-	{path: "/lab2_2", action: lab2_2}
+	{path: "/lab2_2", action: lab2_2},
+	{path: "/lab3_1", action: lab3_1}
 	];
 
 var service = require("./lib/service").http(urlMap);
